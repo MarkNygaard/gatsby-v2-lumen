130cb4b8adaa3b6d8649708a7609c6257a4a0968
Mark Nygaard Leth <mark@MacBook-Pro.localdomain>
2019-02-18 21:04:07 +0100

    .

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
-import Sidebar from '../components/Sidebar'
 import Layout from '../components/Layout'
+import Sidebar from '../components/Sidebar'
 
 class NotFoundRoute extends React.Component {
   render() {
