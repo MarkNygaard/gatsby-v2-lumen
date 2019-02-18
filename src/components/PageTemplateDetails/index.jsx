739af980f1870e8f09a226bb3da33de914be3165
Mark Nygaard Leth <mark@MacBook-Pro.localdomain>
2019-02-18 22:04:41 +0100

    .

--- src/components/PageTemplateDetails/index.jsx
+++ src/components/PageTemplateDetails/index.jsx
@@ -7,7 +7,7 @@ class PageTemplateDetails extends React.Component {
     const page = this.props.data.markdownRemark
 
     return (
-      <div>
+      <>
         <Sidebar {...this.props} />
         <div className="content">
           <div className="content__inner">
@@ -21,7 +21,7 @@ class PageTemplateDetails extends React.Component {
             </div>
           </div>
         </div>
-      </div>
+      </>
     )
   }
 }
